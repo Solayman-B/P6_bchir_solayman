a673e67b456b2237c5bfff624fca0d7c888ff675
Solayman <solayman.b@live.fr>
2021-08-30 16:15:21 +0100

    ajout des photos des films dans le 1er carousel et réécriture du carousel

--- P6_03_javascript.js
+++ P6_03_javascript.js
@@ -1,40 +1,49 @@
 // fetch
 let url = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
+let url2 = url + "&page=2"
 
 fetch(url).then(response => response.json()).then(json_response => {
-    best_movie_title.textContent = json_response.results[0].title
-
+    bandeau_title.textContent = json_response.results[0].title
+    img_bandeau.src = json_response.results[0].image_url
+    a1.src = json_response.results[1].image_url
+    console.log(json_response.results[2].title)
+    a2.src = json_response.results[2].image_url
+    a3.src = json_response.results[3].image_url
+    a4.src = json_response.results[4].image_url
 })
 
-
-
-
+fetch(url2).then(response => response.json()).then(json_response => {
+    a5.src = json_response.results[0].image_url
+    a6.src = json_response.results[1].image_url
+    a7.src = json_response.results[2].image_url
+})
 
 
 // carousel
 document.getElementById("previous1").addEventListener("click", previous1);
 document.getElementById("next1").addEventListener("click", next1);
-let i = -1
-function previous1() {
-    i--;
-    if (i > -3) {
-        console.log(i)
-    document.getElementsByClassName("display1")[i].style.display = "initial";
-    document.getElementsByClassName("hide1")[i].style.display = "none";
-}
-    else {
-        i = -2
+let i = 0
+function toggle_display(i) {
+    let start = "a" + i
+    let stop = "a" + (i + 4)
+    if (document.getElementById(start).style.display === "none") {
+        document.getElementById(start).style.display = "initial";
+        document.getElementById(stop).style.display = "none";
+    } else {
+document.getElementById(start).style.display = "none";
+document.getElementById(stop).style.display = "initial";
     }
 }
 
+function previous1() {
+    i--;
+    if (i >= 0) {
+        toggle_display(i)
+    }
+}
 function next1() {
     i++;
-    if (i < 3) {
-        console.log(i)
-    document.getElementsByClassName("display1")[i].style.display = "none";
-    document.getElementsByClassName("hide1")[i].style.display = "initial";
-}
-    else {
-        i = 3
+    if (i <= 3) {
+        toggle_display(i)
     }
 }
